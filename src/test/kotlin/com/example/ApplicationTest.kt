b819b68ae36c2e0219ea43a435791d7c463e78b2
Gabriel Do Couto Santos <gabrieldocoutosantos@Gabriels-MacBook-Pro.local>
2022-11-19 09:18:35 -0300

    initial commit

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,32 @@
+package com.example
+
+import com.example.plugins.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
+import io.ktor.util.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
